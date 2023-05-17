ea8f3b2c11fb6055f70c6030f40043d313ddbda1
Jonatan <tarriba52@gmail.com>
2023-05-16 19:21:56 -0500

    subiendo img del header

--- org/src/App.js
+++ org/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <img src="/img/header.png" alt="Org"></img>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
